9d25502ce72ce32c1b52ff0977dbb2e01b210f3c
Kanji-Overhill <ingluisfelipe07@gmail.com>
2017-06-26 17:12:51 -0500

    correciones

--- views/modules/servicios.php
+++ views/modules/servicios.php
@@ -1,6 +1,10 @@
 <?php include "menu.php"; ?>
 <main>
-	<section class="container-fluid">
-		
+	<section class="container-fluid servicios-sec-1">
+		<div class="row">
+			<div class="col-12 no-padding-right no-padding-left">
+				<h1>Servicios</h1>
+			</div>
+		</div>
 	</section>
 </main>
